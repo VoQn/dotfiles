aca13c298713edd4fcb51151e671c7b769bbae3e
VoQn <VoQn.tyrantist@gmail.com>
2011-04-18 17:15:57 +0900

    Added README and common Environmental Variables

---
+++ zsh/env_common.sh
@@ -0,0 +1,19 @@
+# Language And Charactor encoding
+# export LC_CTYPE=ja_JP.UTF-8
+export LC_ALL=ja_JP.UTF-8
+export LANG=ja_JP.UTF-8
+
+# PATHs
+# Add SuperUser bin
+PATH=/usr/sbin:/usr/local/sbin:$PATH
+
+# Add Local bin
+PATH=/usr/local/bin:$HOME/bin:$PATH
+MANPATH=/usr/local/man:/usr/share/man
+
+# Java
+export _JAVA_OPTIONS="-Dfile.encoding=UTF-8"
+
+export PATH MANPATH
+
+# EOF
